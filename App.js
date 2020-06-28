69eb2f78369e604d7b0c2e1ee25a088d94a7e712
Bulley,Stephen <Stephen.Bulley@cerner.com>
2020-06-28 18:41:22 -0500

    Added a Tab Navigator and dummy tabs

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import SportList from "./components/sports-list/SportList";
+import * as React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Header } from "react-native-elements";
+import SportList from "./components/sports-list/SportList";
 
-export default function App() {
+function BetScreen() {
   return (
     <View style={styles.container}>
       <Header
@@ -24,7 +25,37 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "grey",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+function ProfileScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Profile Screen</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Bets" component={BetScreen} />
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
